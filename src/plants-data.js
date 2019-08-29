8e37a9f46f1944ac7e48d2bcafb0c8e743373944
Ashley <ashleywong678@gmail.com>
2019-08-29 12:14:08 -0400

    added value for scientific name and a regular name

--- src/plants-data.js
+++ src/plants-data.js
@@ -1,8 +1,8 @@
-const plants = [{url: "http://www.costafarms.com/CostaFarms/Agave-Desert-Escape-Costa-Farms-Succulent1.jpg?height=257&width=256&scale=both&crop=auto", name: "Agave desmettiana"},
-{url: "http://www.costafarms.com/CostaFarms/Costa-Farms-Succulent-Agave-ferox.jpg?height=257&width=256&scale=both&crop=auto", name: "Agave salmiana"},
-{url: "http://www.costafarms.com/CostaFarms/Aloe-Aristata-costa-farms-succulent.jpg?height=257&width=256&scale=both&crop=auto", name: "Aloe aristata"},
-{url: "http://www.costafarms.com/CostaFarms/Costa-Farms-Succulent-Aloe-brevifolia.jpg?height=257&width=256&scale=both&crop=auto", name: "Aloe brevifolia"},
-{url: "http://www.costafarms.com/CostaFarms/Aloe-Bright-Star-costa-farms-succulent.jpg?height=257&width=256&scale=both&crop=auto", name: "Aloe 'Bright Star'"},
-{url: "http://www.costafarms.com/CostaFarms/Aloe-Cha-Cha-costa-farms-succulent1.jpg?height=257&width=256&scale=both&crop=auto", name: "Aloe 'Cha Cha'"}]
+const plants = [{url: "http://www.costafarms.com/CostaFarms/Agave-Desert-Escape-Costa-Farms-Succulent1.jpg?height=257&width=256&scale=both&crop=auto", name: "Spikey",sciName: "Agave desmettiana"},
+{url: "http://www.costafarms.com/CostaFarms/Costa-Farms-Succulent-Agave-ferox.jpg?height=257&width=256&scale=both&crop=auto", name: "George",sciName: "Agave salmiana"},
+{url: "http://www.costafarms.com/CostaFarms/Aloe-Aristata-costa-farms-succulent.jpg?height=257&width=256&scale=both&crop=auto", name: "Spots",sciName: "Aloe aristata"},
+{url: "http://www.costafarms.com/CostaFarms/Costa-Farms-Succulent-Aloe-brevifolia.jpg?height=257&width=256&scale=both&crop=auto", name: "Jaws",sciName: "Aloe brevifolia"},
+{url: "http://www.costafarms.com/CostaFarms/Aloe-Bright-Star-costa-farms-succulent.jpg?height=257&width=256&scale=both&crop=auto", name: "Tiny",sciName: "Aloe 'Bright Star'"},
+{url: "http://www.costafarms.com/CostaFarms/Aloe-Cha-Cha-costa-farms-succulent1.jpg?height=257&width=256&scale=both&crop=auto", name: "Felica",sciName: "Aloe 'Cha Cha'"}]
 
 export default plants

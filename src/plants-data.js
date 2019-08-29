4954d0ee64c7dc7d07e07e3ab9ab5ba2339d7f4f
Ashley <ashleywong678@gmail.com>
2019-08-29 11:58:08 -0400

    added seed data

---
+++ src/plants-data.js
@@ -0,0 +1,8 @@
+const plants = [{url: "http://www.costafarms.com/CostaFarms/Agave-Desert-Escape-Costa-Farms-Succulent1.jpg?height=257&width=256&scale=both&crop=auto", name: "Agave desmettiana"},
+{url: "http://www.costafarms.com/CostaFarms/Costa-Farms-Succulent-Agave-ferox.jpg?height=257&width=256&scale=both&crop=auto", name: "Agave salmiana"},
+{url: "http://www.costafarms.com/CostaFarms/Aloe-Aristata-costa-farms-succulent.jpg?height=257&width=256&scale=both&crop=auto", name: "Aloe aristata"},
+{url: "http://www.costafarms.com/CostaFarms/Costa-Farms-Succulent-Aloe-brevifolia.jpg?height=257&width=256&scale=both&crop=auto", name: "Aloe brevifolia"},
+{url: "http://www.costafarms.com/CostaFarms/Aloe-Bright-Star-costa-farms-succulent.jpg?height=257&width=256&scale=both&crop=auto", name: "Aloe 'Bright Star'"},
+{url: "http://www.costafarms.com/CostaFarms/Aloe-Cha-Cha-costa-farms-succulent1.jpg?height=257&width=256&scale=both&crop=auto", name: "Aloe 'Cha Cha'"}]
+
+export default plants

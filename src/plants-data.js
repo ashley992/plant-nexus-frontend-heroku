45c5478d525e95ef938b8cde48ccb75bea0eeda1
Ashley <ashleywong678@gmail.com>
2019-08-29 18:49:40 -0400

    added scientific name attribute

--- src/plants-data.js
+++ src/plants-data.js
@@ -1,8 +1,8 @@
-const plants = [{url: "http://www.costafarms.com/CostaFarms/Agave-Desert-Escape-Costa-Farms-Succulent1.jpg?height=257&width=256&scale=both&crop=auto", name: "Spikey",sciName: "Agave desmettiana"},
-{url: "http://www.costafarms.com/CostaFarms/Costa-Farms-Succulent-Agave-ferox.jpg?height=257&width=256&scale=both&crop=auto", name: "George",sciName: "Agave salmiana"},
-{url: "http://www.costafarms.com/CostaFarms/Aloe-Aristata-costa-farms-succulent.jpg?height=257&width=256&scale=both&crop=auto", name: "Spots",sciName: "Aloe aristata"},
-{url: "http://www.costafarms.com/CostaFarms/Costa-Farms-Succulent-Aloe-brevifolia.jpg?height=257&width=256&scale=both&crop=auto", name: "Jaws",sciName: "Aloe brevifolia"},
-{url: "http://www.costafarms.com/CostaFarms/Aloe-Bright-Star-costa-farms-succulent.jpg?height=257&width=256&scale=both&crop=auto", name: "Tiny",sciName: "Aloe 'Bright Star'"},
-{url: "http://www.costafarms.com/CostaFarms/Aloe-Cha-Cha-costa-farms-succulent1.jpg?height=257&width=256&scale=both&crop=auto", name: "Felica",sciName: "Aloe 'Cha Cha'"}]
+const plants = [{img_url: "http://www.costafarms.com/CostaFarms/Agave-Desert-Escape-Costa-Farms-Succulent1.jpg?height=257&width=256&scale=both&crop=auto", name: "Spikey",sciName: "Agave desmettiana"},
+{img_url: "http://www.costafarms.com/CostaFarms/Costa-Farms-Succulent-Agave-ferox.jpg?height=257&width=256&scale=both&crop=auto", name: "George",sciName: "Agave salmiana"},
+{img_url: "http://www.costafarms.com/CostaFarms/Aloe-Aristata-costa-farms-succulent.jpg?height=257&width=256&scale=both&crop=auto", name: "Spots",sciName: "Aloe aristata"},
+{img_url: "http://www.costafarms.com/CostaFarms/Costa-Farms-Succulent-Aloe-brevifolia.jpg?height=257&width=256&scale=both&crop=auto", name: "Jaws",sciName: "Aloe brevifolia"},
+{img_url: "http://www.costafarms.com/CostaFarms/Aloe-Bright-Star-costa-farms-succulent.jpg?height=257&width=256&scale=both&crop=auto", name: "Tiny",sciName: "Aloe 'Bright Star'"},
+{img_url: "http://www.costafarms.com/CostaFarms/Aloe-Cha-Cha-costa-farms-succulent1.jpg?height=257&width=256&scale=both&crop=auto", name: "Felica",sciName: "Aloe 'Cha Cha'"}]
 
 export default plants

a0ad820af93108615cec84187409a8817df17bb1
Ashley <ashleywong678@gmail.com>
2019-08-29 18:48:08 -0400

    added render for scientific name vs plant name

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -4,7 +4,7 @@ const PlantCard = ({ plant }) => {
 
   return (
     <>
-      <img src={plant.url} alt={plant.sciName}/>
+      <img src={plant.img_url} alt={plant.sciName}/>
       <p>Name: {plant.name}</p>
       <p>Scientific Name: {plant.sciName}</p>
     </>

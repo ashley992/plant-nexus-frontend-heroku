32b9067b3f298bfff20922a870effc02ebfeaab4
Ashley <ashleywong678@gmail.com>
2019-08-29 18:46:15 -0400

    moved local store and component mount here, updated rendered component props

--- src/App.js
+++ src/App.js
@@ -2,14 +2,36 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Plants from './components/Plants'
+import DisplayPlant from './components/DisplayPlant'
+import plants from './plants-data'
+
 
 class App extends Component {
+
+  constructor(){
+    super()
+    this.state = {
+      plants: []
+    }
+  }
+
+  componentDidMount(){
+    this.setState({
+      plants: plants
+    })
+  }
+
   render(){
       return (
         <div className="App">
           <h1>Plant Nexus</h1>
-          <br />
-          <Plants />
+          { this.state.plants.length === 0 ?
+            "Loading..." :
+            <div>
+              <DisplayPlant plant={this.state.plants[0]}/>
+              <Plants plants={this.state.plants}/>
+            </div>
+          }
       </div>
     );
   }

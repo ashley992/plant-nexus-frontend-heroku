e5aa641ec800b03e175269525a57145d80ff69fb
Ashley <ashleywong678@gmail.com>
2019-08-30 16:19:34 -0400

    imported users reducer

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
-import usersReducer from './reducers/users'
+import usersReducer from './reducers/usersReducer'
 
 const reducer = combineReducers({
   users: usersReducer

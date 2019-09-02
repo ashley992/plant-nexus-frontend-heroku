c697da4d03d3152231580c2f50f121b3bd5c57e7
Ashley <ashleywong678@gmail.com>
2019-09-02 12:36:43 -0400

    moved components around for styling purposes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { connect } from 'react-redux'
 import { getCurrentUser } from './actions/currentUser.js'
 import NavBar from './components/NavBar.js'
+import MainContainer from './components/MainContainer.js'
+
 // import Plants from './components/Plants'
 // import DisplayPlant from './components/DisplayPlant'
 // import plants from './plants-data'
@@ -18,8 +20,8 @@ class App extends Component {
   render(){
       return (
         <div>
-          <h1 className="center">Plant Nexus</h1>
           <NavBar />
+          <h1 className="center">Plant Nexus</h1>
           <MainContainer />
           {/* <Footer /> */}
       </div>

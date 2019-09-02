41be904d82ccf4e9b7c7111d24bc95157e7d6fbe
Ashley <ashleywong678@gmail.com>
2019-09-02 12:06:45 -0400

    moved login, loggout, and mapStateToProps to NavBar component and rendered navbar in App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import Login from './components/Login.js'
-import Logout from './components/Logout.js'
 import { connect } from 'react-redux'
 import { getCurrentUser } from './actions/currentUser.js'
+import NavBar from './components/NavBar.js'
 // import Plants from './components/Plants'
 // import DisplayPlant from './components/DisplayPlant'
 // import plants from './plants-data'
@@ -18,19 +17,15 @@ class App extends Component {
 
   render(){
       return (
-        <div className="App">
-          <h1>Plant Nexus</h1>
-          {this.props.currentUser ? <Logout /> : <Login />}
+        <div>
+          <h1 className="center">Plant Nexus</h1>
+          <NavBar />
+          <MainContainer />
+          {/* <Footer /> */}
       </div>
     );
   }
 }
 
-const mapStateToProps = ({ currentUser }) => {
-  //currentUser deconstructed from incoming object and we know it has a property called currentUser
-  return {
-    currentUser
-  }
-}
 
-export default connect(mapStateToProps, { getCurrentUser })(App);
+export default connect(null, { getCurrentUser })(App);

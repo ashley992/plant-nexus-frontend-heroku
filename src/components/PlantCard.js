473da1be883bdcda37a01dd88c47a27836d1a205
Ashley <ashleywong678@gmail.com>
2019-09-03 09:29:47 -0400

    changed wrapped element to a div so easier to add styling and keep text together with pricture when rendered in grid form

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -3,10 +3,10 @@ import React from 'react'
 const PlantCard = ({plant}) =>{
 // console.log(plant)
   return (
-    <>
+    <div>
       <img src={plant.image_url} alt={plant.name}/>
       <p>Name: {plant.name}</p>
-    </>
+    </div>
   )
 }
 

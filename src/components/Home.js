3b4e838b9bb57873200d5c0a0278c7be0cf904f7
Ashley <ashleywong678@gmail.com>
2019-09-04 17:23:28 -0400

    import login, signup, link, added a heading to render with links to forms

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import Login from './Login'
+import Signup from './Signup'
+import { Link } from 'react-router-dom'
+
 
 const Home = () => {
-  return ()
+  return (
+    <div className="home">
+      <h2>Welcome, please <Link to='/signup'>Signup</Link> or <Link to='/login'>Login</Link></h2>
+    </div>
+  )
 }
 
 export default Home

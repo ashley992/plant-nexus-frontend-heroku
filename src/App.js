7b129d257f5681233ddb310a2b4ab63f6e8d6bf4
Ashley <ashleywong678@gmail.com>
2019-09-04 22:25:04 -0400

    commented out logout b/c used in navbar, removed const loggedIn b/c passed as props, updated / path to reflect full props, edited signup route to pass history so it is able to render after signup successful

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { getCurrentUser } from './actions/currentUser.js'
 import NavBar from './components/NavBar.js'
 import Home from './components/Home.js'
 import Login from './components/Login.js'
-import Logout from './components/Logout.js'
+// import Logout from './components/Logout.js'
 import Signup from './components/Signup.js'
 import MyPlants from './components/MyPlants.js'
 // import MainContainer from './components/MainContainer.js'
@@ -16,17 +16,16 @@ class App extends Component {
   componentDidMount(){
     this.props.getCurrentUser()
   }
-
   render(){
-    const loggedIn = this.props
       return (
         <div className='center'>
           {/* <Switch> */}
             <NavBar />
             {/* <MainContainer /> */}
-            <Route exact path='/' render={() => loggedIn ? <MyPlants /> : <Home />} />
+            <Route exact path='/' render={() => this.props.loggedIn ? <MyPlants /> : <Home />} />
             <Route exact path='/login' component={Login}/>
-            <Route exact path='/signup' component={Signup}/>
+            <Route exact path='/signup' render={({history})=> <Signup history={history}/>} />
+
             <Route exact path='/plants' component={MyPlants}/>
           {/* </Switch> */}
         </div>

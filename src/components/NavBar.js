df7f58936a67ca56830c8a1dfddbfddd2f8b79bc
Ashley <ashleywong678@gmail.com>
2019-09-05 17:06:30 -0400

    added link for new plant form

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 // import Login from './Login.js'
 import Logout from './Logout.js'
 // import Signup from './Signup.js'
+import { Link } from 'react-router-dom'
 
 
 const NavBar = ({ currentUser, loggedIn }) => {
@@ -10,6 +11,7 @@ const NavBar = ({ currentUser, loggedIn }) => {
   return (
     <div className="navbar">
       <h3 className='logoName'>PlantNexus</h3> 
+      <h4><Link to='/my-plants/new'>New Plant</Link></h4>
       {/* <h4>Hi {currentUser.attributes.name}!</h4>     */}
       { loggedIn ? <Logout/> : null }
     </div>

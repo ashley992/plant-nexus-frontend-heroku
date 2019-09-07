92c8617937517191bafcdded06b46abe2c9aae5c
Ashley <ashleywong678@gmail.com>
2019-09-06 23:53:21 -0400

    spacing changes

--- src/reducers/myPlants.js
+++ src/reducers/myPlants.js
@@ -1,11 +1,15 @@
 export default (state = [], action) => {
   switch (action.type) {
-
+    
     case 'SET_MY_PLANTS':
-      return state = [...state, action.plant]
-
-    case 'CLEAR_MY_PLANTS':
-      return []
+      return [...state, action.plants]
+      
+      case 'CLEAR_MY_PLANTS':
+        return []
+        
+      case 'ADD_CREATED_PLANT':
+        console.log("in reducer", action)
+        return state = [...state, action.plant]
 
     default:
       return state

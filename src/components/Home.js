e4c2db1a1381c8ef6de15a19cee3d7d6dce47aa5
Ashley <ashleywong678@gmail.com>
2019-09-06 23:37:30 -0400

    removed commented out imports, imported connect, got current user from state to props, added condition to heading to show link to home if looged in but signup/login if no current user

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,23 @@
 import React from 'react'
-// import Login from './Login'
-// import Signup from './Signup'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 
-const Home = () => {
+const Home = ({ currentUser }) => {
+  
   return (
     <div className="home">
-      <h2>Welcome, please <Link to='/signup'>Signup</Link> or <Link to='/login'>Login</Link></h2>
+      <h2>
+        Welcome, { currentUser ? <>go to <Link to='/my-plants'>My Plants</Link></> : <>please <Link to='/signup'>Signup</Link> or <Link to='/login'>Login</Link></>}
+      </h2>
     </div>
   )
 }
 
-export default Home
+const mapStateToProps = ({ currentUser }) => {
+  return {
+    currentUser
+  }
+}
+
+export default connect(mapStateToProps)(Home)

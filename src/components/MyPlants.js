2d7d86faae8e076b74b6857af48dbefae3a4c3d4
Ashley <ashleywong678@gmail.com>
2019-09-06 23:40:18 -0400

    removed console.log

--- src/components/MyPlants.js
+++ src/components/MyPlants.js
@@ -4,17 +4,17 @@ import PlantCard from './PlantCard'
 
 const MyPlants = ({props}) => {
  //props.myPlants = [ {a plant}, {another plant}]
-//  console.log(props.myPlants)
+
   const plantCards = props.myPlants.length > 0 ? 
   props.myPlants.map(plant => <PlantCard plant={plant} key={plant.id} />) : null
 
   return (
-    <>
+    <div>
       {props.currentUser ? <h3>You're current plants:</h3> : ""}
       <div className="grid">
         {plantCards}
       </div>
-    </>
+    </div>
   )
 }
 

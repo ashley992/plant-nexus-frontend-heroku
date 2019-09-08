113ea7726c4eab3965a08669dd876cd5cafc4c51
Ashley <ashleywong678@gmail.com>
2019-09-08 11:13:04 -0400

    removed unused imports to clean up document

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import { connect } from 'react-redux'
-// import Logout from './Logout.js'
-// import { NavLink } from 'react-router-dom'
 import LoggedInNavBar from './LoggedInNavbar'
 import LoggedOutNavBar from './LoggedOutNavBar'
 

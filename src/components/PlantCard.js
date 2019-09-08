b9434cc5782e9f09925b7b54bf07035a9117c22a
Ashley <ashleywong678@gmail.com>
2019-09-08 16:28:27 -0400

    if plant found, renders plant info and is linked to show page, if no plant found then renders sad text

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const PlantCard = ({plant}) =>{
 // console.log(plant)
   return (
-    <div>
-      <img src={plant.image_url} alt={plant.name}/>
-      <p>Name: {plant.name}</p>
-    </div>
+    trip ? 
+      <div>
+        <Link to=`/my-plants/${plant.id}`>
+          <img src={plant.image_url} alt={plant.name}/>
+          <p>Name: {plant.name}</p>
+        </Link>  
+        <Link to={`/my-plants/${plant.id}/edit`}> Edit this plant </Link>
+      </div> :
+      <p>This plant either died or wasn't found :(</p>
   )
 }
 

ff33ac0b6f36c0658aa1d8ff9db55c972706bd2c
Ashley <ashleywong678@gmail.com>
2019-09-08 11:09:10 -0400

    cleaned up file, removed unused components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import './App.css';
 import { connect } from 'react-redux'
 import { getCurrentUser } from './actions/currentUser.js'
 import NavBar from './components/NavBar.js'
-// import LoggedOutNavBar from './components/LoggedOutNavBar'
 import Login from './components/Login.js'
 import Signup from './components/Signup.js'
 import MyPlants from './components/MyPlants.js'
-// import MainContainer from './components/MainContainer.js'
-import { Route, withRouter, Switch, Link } from 'react-router-dom'
+import { Route, withRouter, Switch} from 'react-router-dom'
 import NewPlantForm from './components/NewPlantForm'
 
 class App extends Component {
@@ -17,14 +15,12 @@ class App extends Component {
     this.props.getCurrentUser()
   }
   render(){
-    const {loggedIn} = this.props
       return (
         <div className='center'>
-          {/* { loggedIn ? <NavBar /> : <LoggedOutNavBar />} */}
+          <NavBar />
 
           <Switch>
-            {/* <MainContainer /> */}
-            <Route exact path='/' component={NavBar} />
+            {/* <Route exact path='/' component={NavBar} /> */}
             <Route exact path='/login' component={Login}/>
             <Route exact path='/signup' render={({history})=> <Signup history={history}/>} />
             <Route exact path='/my-plants' component={MyPlants}/>

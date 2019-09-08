f60726dcd52bd38a0292ec53423142a1f19a2787
Ashley <ashleywong678@gmail.com>
2019-09-08 12:57:50 -0400

    removed component did mount, changed mind

--- src/components/MyPlants.js
+++ src/components/MyPlants.js
@@ -6,10 +6,6 @@ import { getPlants } from '../actions/myPlants'
 const MyPlants = ({props}) => {
  //props.myPlants = [ {a plant}, {another plant}]
 
-  const componentDidMount = () => {
-    console.log(props)
-  }
-
   const plantCards = props.myPlants.length > 0 ? 
   props.myPlants.map(plant => <PlantCard plant={plant} key={plant.id} />) : null
 

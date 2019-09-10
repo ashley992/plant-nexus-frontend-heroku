ef0f0c61a3b93df28d7e6bf09474e9a5feed3470
Ashley <ashleywong678@gmail.com>
2019-09-10 13:18:15 -0400

    changed passed in data name in update plant form action, added reset plantForm action, built setEditForm action

--- src/actions/plantForm.js
+++ src/actions/plantForm.js
@@ -1,6 +1,25 @@
-export const updatePlantForm = plantData => {
+export const updatePlantForm = formData => {
   return {
     type: "UPDATE_NEW_PLANT_FORM",
-    plantData
+    formData
+  }
+}
+
+export const resetPlantForm = () => {
+  return {
+    type: "RESET_NEW_PLANT_FORM",
+  }
+}
+
+export const setEditFormData = plant => {
+  console.log("in set edit form", plant)
+  const plantFormData = {
+    name: plant.attributes.name,
+    scientificName: plant.attributes.scientific_name,
+    imageUrl: plant.attributes.image_url
+  }
+  return {
+    type: "SET_EDIT_FORM_DATA",
+    plantFormData
   }
 }

199756e30afbec884c660ed25a9ca99f2f6981b9
Ashley <ashleywong678@gmail.com>
2019-09-10 22:15:01 -0400

    removed console.logs, fixed typos from trip to plant, updated links to updated route, changed error message and added a sad plant to error

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -2,17 +2,20 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const PlantCard = ({plant}) =>{
-// console.log(plant)
   return (
-    trip ? 
+    plant ? 
       <div>
-        <Link to=`/my-plants/${plant.id}`>
+        <Link to={`/plants/${plant.id}`}>
           <img src={plant.image_url} alt={plant.name}/>
           <p>Name: {plant.name}</p>
-        </Link>  
-        <Link to={`/my-plants/${plant.id}/edit`}> Edit this plant </Link>
+          <p>Scientific Name: {plant.scientific_name}</p>
+        </Link>
+        <Link to={`/plants/${plant.id}/edit`}> Edit this plant </Link>
       </div> :
-      <p>This plant either died or wasn't found :(</p>
+      <div>
+        <h2>Sorry, this plant either died or wasn't found :(</h2>
+        <img id='sadPlant' src="https://res.cloudinary.com/teepublic/image/private/s--wMHW3OpX--/t_Preview/b_rgb:36538b,c_limit,f_jpg,h_630,q_90,w_630/v1446238240/production/designs/268839_1.jpg" alt="Sad Plant"/>
+      </div>
   )
 }
 

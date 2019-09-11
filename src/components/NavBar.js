141665408d20c1ee7538ed3d1c46e1507370e34d
Ashley <ashleywong678@gmail.com>
2019-09-11 14:18:47 -0400

    imported link to link logo and logo name to / route

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { connect } from 'react-redux'
 import LoggedInNavBar from './LoggedInNavbar'
 import LoggedOutNavBar from './LoggedOutNavBar'
+import { Link } from 'react-router-dom'
 
 
 const NavBar = ({ currentUser, loggedIn }) => {
 
   return (
-    <div className="navbar">
-      <h3 className='logoName'> PlantNexus </h3>
+    <div className="navbar center">
+      <Link to='/'><h3 className='logoName'> <img className='navPic' src='logo-2.png' alt='plant-logo'/>PlantNexus </h3></Link>
       { loggedIn ? <LoggedInNavBar /> : <LoggedOutNavBar />}
     </div>
   )

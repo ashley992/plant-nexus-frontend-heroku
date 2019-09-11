01b2d057b9c254d5bb0e1c0857dae57d373623b1
Ashley <ashleywong678@gmail.com>
2019-09-11 17:46:49 -0400

    fixed bug with misspelled className

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
 
   return(
-    <div className>
+    <div className='home'>
       <img className='logoPic' src='logo-2.png' alt='plant-logo'/>
       <h1>Welcome to PlantNexus</h1>
     </div>

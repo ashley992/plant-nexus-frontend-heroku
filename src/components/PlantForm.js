4d058224c190f4b685965893edd74ed1efbbb04f
Ashley <ashleywong678@gmail.com>
2019-09-11 16:58:21 -0400

    added text area for notes section

--- src/components/PlantForm.js
+++ src/components/PlantForm.js
@@ -18,22 +18,16 @@ const PlantForm = ({ formData, history, updatePlantForm, handleSubmit, editMode,
       event.preventDefault()
       handleSubmit(formData, history)
     }}>
-      <input
-        placeholder="name" name="name" onChange={handleChange} value={formData.name}
-      />
+      <input placeholder="name" name="name" onChange={handleChange} value={formData.name} />
         <br/>
-      <input
-        placeholder="Scientific Name" name="scientificName"
-        onChange={handleChange} value={formData.scientificName}
-      />
+      <input placeholder="Scientific Name" name="scientificName"
+        onChange={handleChange} value={formData.scientificName} />
       <br/>
-      <input
-        placeholder="Image Url" name="imageUrl" onChange={handleChange} value={formData.imageUrl}
-      />
+      <input placeholder="Image Url" name="imageUrl" onChange={handleChange} value={formData.imageUrl}/>
       <br/>
-      <input
-        type="submit" value={editMode ? "Update Plant" : "Create Plant" }
-      />
+      <textarea rows='5' columns='40' placeholder="Notes" name="notes" onChange={handleChange} value={formData.notes}/>
+      <br />
+      <input type="submit" value={editMode ? "Update Plant" : "Create Plant" }/>
     </form>
   );
 }

018717161e6a9afb27fccfccb99aed68533dd666
Ashley <ashleywong678@gmail.com>
2019-09-11 14:14:43 -0400

    added a home page for / route

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+
+  return(
+    <div className>
+      <img className='logoPic' src='logo-2.png' alt='plant-logo'/>
+      <h1>Welcome to PlantNexus</h1>
+    </div>
+  )
+}
+export default Home

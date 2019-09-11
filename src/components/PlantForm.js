03681d2d465ab4c30dc70bfa284334a1292a8dcc
Ashley <ashleywong678@gmail.com>
2019-09-10 22:17:28 -0400

    removed passed in props userId and changed typo trip to plant, changed handleChange to accept an object, updated form values based on deconstructed props, removed userId passed to state

--- src/components/PlantForm.js
+++ src/components/PlantForm.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { updatePlantForm } from '../actions/plantForm'
 import { connect } from 'react-redux'
 
-const PlantForm = ({ formData, updatePlantForm, userId, trip, handleSubmit, editMode }) => {
-
-  const { name, scientificName, imageUrl } = formData
-
+const PlantForm = ({ formData, updatePlantForm, handleSubmit, editMode, plant }) => {
 
   const handleChange = event => {
     const { name, value } = event.target
-    updatePlantForm(name, value)
+    const newFormData = {
+      ...formData,
+      [name]: value
+    }
+    updatePlantForm(newFormData)
   }
 
   return (
@@ -18,30 +19,29 @@ const PlantForm = ({ formData, updatePlantForm, userId, trip, handleSubmit, edit
       handleSubmit(formData)
     }}>
       <input
-        placeholder="name" name="name" onChange={handleChange} value={name}
+        placeholder="name" name="name" onChange={handleChange} value={formData.name}
       />
         <br/>
       <input
         placeholder="Scientific Name" name="scientificName"
-        onChange={handleChange} value={scientificName}
+        onChange={handleChange} value={formData.scientificName}
       />
       <br/>
       <input
-        placeholder="Image Url" name="imageUrl" onChange={handleChange} value={imageUrl}
+        placeholder="Image Url" name="imageUrl" onChange={handleChange} value={formData.imageUrl}
       />
       <br/>
       <input
-        type="submit" value={editMode ? "Update Plant" : "Create Trip" }
+        type="submit" value={editMode ? "Update Plant" : "Create Plant" }
       />
     </form>
   );
 }
 
 const mapStateToProps = state => {
-  const userId = state.currentUser ? state.currentUser.id : ""
+  // const userId = state.currentUser ? state.currentUser.id : ""
   return {
     formData: state.plantForm,
-    userId
   }
 }
 

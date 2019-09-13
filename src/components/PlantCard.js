cf45dc6726a472f29b6919ec254cdc9ae7e39cf6
Ashley <ashleywong678@gmail.com>
2019-09-13 19:24:00 -0400

    added classes for styling text and default pic render if no pic added

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -2,16 +2,19 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const PlantCard = ({plant}) =>{
+
   return (
     plant ? 
-      <div>
+      <div className="plant-card">
         <Link to={`/plants/${plant.id}`}>
-          <img src={plant.image_url} alt={plant.name}/>
-          <p>Name: {plant.name}</p>
-          <p>Scientific Name: {plant.scientific_name}</p>
-          <p>Notes: {plant.notes}</p>
+          {plant.image_url === "" ? <img id='happy-cactus' src='/happy-cactus.png' alt='empty plant pic'/> : <img className="uploaded" src={plant.image_url} alt={plant.name}/>}
+          <div className='plant-card-text'>
+            <p><strong>Name:</strong> {plant.name}</p>
+            <p><strong>Scientific Name:</strong> {plant.scientific_name}</p>
+            <p><strong>Notes:</strong> {plant.notes}</p>
+          </div>
         </Link>
-        <Link to={`/plants/${plant.id}/edit`}> Edit this plant </Link>
+        <Link to={`/plants/${plant.id}/edit`}><strong>Edit this plant</strong></Link>
       </div> :
       <div>
         <h2>Sorry, this plant either died or wasn't found :(</h2>

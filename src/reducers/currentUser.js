cecec5c3c1646b785f4fb0e2636dc9e58ed01096
Ashley <ashleywong678@gmail.com>
2019-10-13 22:15:53 -0400

    updated current user after rails updates

--- src/reducers/currentUser.js
+++ src/reducers/currentUser.js
@@ -2,6 +2,11 @@ export default (state = null, action) => {
   switch (action.type) {
     case "SET_CURRENT_USER":
       return action.user
+    case "UPDATE_CURRENT_USER":
+      const attr = {name: action.user.name, username: action.user.username}
+      return state = {...state,
+        attributes: attr
+      }
     case "CLEAR_CURRENT_USER":
       return null
     default:
